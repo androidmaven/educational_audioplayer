102856f2da24d020397f666d4ea62a8616606597
Marat Gadzhibutaev <rabbitjkee@gmail.com>
2019-06-07 19:12:21 +0300

    update alert messages

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -9,25 +9,25 @@ const double closeIconSize = 32.0;
 const String downloadAudiosTitle = 'Загрузка файлов';
 const String deleteAudiosTitle = 'Удаление файлов';
 const String loadingConfirmationTitle_1 =
-    'Уверены, что хотите скачать аудиофайлы \n(';
+    'Уверены, что хотите загрузить аудиофайлы \n(';
 const String loadingConfirmationTitle_2 = ' Мбайт)';
 const String deletionConfirmationTitle =
-    'Уверены, что хотите удалить аудиофайлы?';
+    'Уверены, что хотите удалить загруженные аудтофайлы?';
 const String loadingConfirmationInfo =
-    'При медленном интернете это может занять значительное время (час и более)';
+    'При медленном интернет-соединении это может занять много времени';
 const String deletionConfirmationInfo =
-    'Для повторного прослушивания аудиофайлов их придется скачать заново';
-const String confirmLoadingButtonText = 'Скачать';
+    'Для повторного прослушивания их придется загрузить заново или слушать онлайн (для этого нужно постоянное подключение к интернету)';
+const String confirmLoadingButtonText = 'Загрузить';
 const String confirmDeletionButtonText = 'Удалить';
 const String closeDialogButtonText = 'Отмена';
 const String loadingFailedDialogTitle = 'Не удалось загрузить аудио';
 const String loadingFailedDialogInfo =
     'Возможно, нет соединения с Интернетом или недостаточно свободной памяти на устройстве';
 const String deletionFailedDialogInfo =
-    'Не удалось удалить некоторые аудиофайлы. Возможно, они переименованы, удалены или используются другим приложением.';
+    'Не удалось удалить некоторые аудиофайлы. Возможно, они переименованы или удалены.';
 const String playFailedDialogTitle = 'Не удалось воспроизвести аудио';
 const String playFailedDialogInfo =
-    'Возможно, файл поврежден, попробуйде удалить и заново скачать аудио';
+    'Возможно, файл поврежден, попробуйде удалить и заново загрузить аудио';
 const String pressToOpenAudio = 'Нажмите, чтобы открыть аудио';
 
 const String resourceAllDownloadedAudio = 'AllDownloadedAudios';
